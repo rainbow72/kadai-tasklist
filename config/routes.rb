c3278d778ab0df1a1146eef5a1532d8a93b99e3d
golf.athlete <fortechachademy@gmail.com>
2021-06-12 10:26:57 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: "toppages#index"
   
-  get "login", to: "sessions#new"
-  post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
-  
-  resources :tasks
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :create]
 end
