ce3649f2c6a0985e183c44c454ec1671da073a8e
golf.athlete <fortechachademy@gmail.com>
2021-06-16 12:01:21 +0000

    fix the appearance of error page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,11 +6,17 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find_by(params[:id])
+    if nil_task?
+      redirect_to root_url
+    end
   end
 
   def new
     @task = current_user.tasks.build
+    if nil_task?
+      redirect_to root_url
+    end
   end
 
   def create
@@ -25,11 +31,17 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find_by(params[:id])
+    if nil_task?
+      redirect_to root_url
+    end
   end
 
   def update
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find_by(params[:id])
+    if nil_task?
+      redirect_to root_url
+    end
     
     if @task.update(task_params)
       flash[:success] = "タスクが正常に更新されました"
@@ -41,7 +53,10 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find_by(params[:id])
+    if nil_task?
+      redirect_to root_url
+    end
     @task.destroy
     
     flash[:success] = "タスクは正常に削除されました"
@@ -52,6 +67,10 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def nil_task?
+    return !@task
   end
 
 end
